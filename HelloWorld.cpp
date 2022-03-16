bbaed3cc5dc12248801ede9afa4ba59f11bf02c4
Rudrrayan manna <47331292+rudrrayan@users.noreply.github.com>
2022-03-16 11:46:12 +0000

    obfuscate code for better readability
    I propose that the code should be amended to improve contributer readability. In such a case, not only will the ability to easily understand code will be increased, too will the long-term maintainability of the code across future releases of STL and the c++ paradigm.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+// define macros
+#define TEXT "Hello world!"
+#define EXIT 0
+
 using namespace std;
 
+// prints a string manually written into the file
 int main() 
 {
-    cout << "Hello world!";
-    return 0;
+    try{
+        if(true)
+            cout << TEXT << endl;
+        else
+            throw("oh no")
+        
+    }
+    catch(ex){
+        cout << ex << endl;
+    }
+    return EXIT;
 }
